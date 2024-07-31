13b049843605cc76c45fe57052591f4c7586b7b6
Viral Rupani <viralrupani12017@gmail.com>
2024-07-31 23:48:48 +0530

    Tower of Hanoi

---
+++ recursion/TowerOfHanoi/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void TOH(int n, int a, int b, int c)
+{
+    if (n > 0)
+    {
+        TOH(n - 1, a, c, b);
+        printf("(%d, %d)\n", a, b);
+        TOH(n - 1, b, a, c);
+    }
+}
+
+int main()
+{
+    TOH(4, 1, 2, 3);
+    return 0;
+}
