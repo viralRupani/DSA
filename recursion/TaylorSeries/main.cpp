d9d8c90c4a68cf56dc5cc177d613c58731dce09c
viral rupani <viralrupani12017@gmail.com>
2025-08-19 22:12:10 +0530

    Optimized tailor series

--- recursion/TaylorSeries/main.cpp
+++ recursion/TaylorSeries/main.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+// Tailor Series:  1 + x/1! + x^2/2! + x^3/3! + x^4/4!
+// Optimized version: 1 + x/1 [ 1 + x/2 [ 1 + x/3 [ 1 + x/4 [1] ] ] ]
 
+#include <iostream>
 using namespace std;
+
+double optimizedTailorSeriesIterative(double e, double x) {
+    double s = 1;
+
+    for (;x > 0; x--) {
+        s = 1 + ((e / x) * s);
+    }
+
+    return s;
+}
+
+double optimizedTailorRecursive(double e, double x) {
+    static double s = 1;
+
+    if (x == 0) {
+        return s;
+    }
+
+    s = 1 + ((e/x) * s);
+    return optimizedTailorRecursive(e, x - 1);
+}
+
 
 double tailorSeriesRecursive(int e, int x) {
     static double p=1, f=1;
@@ -16,7 +40,7 @@ double tailorSeriesRecursive(int e, int x) {
 }
 
 int main() {
-    double result = tailorSeriesRecursive(4, 10);
+    double result = optimizedTailorSeriesIterative(1, 10);
     printf("%lf", result);
     return 0;
 }
